43572af187dcc9954f0dcdf24aa1f31561a8aa81
Hari Raj Vijayakumar <Hariraj.vijayakumar@vantiva.net>
2022-10-17 17:56:56 +0530

    Remove WRITE_SETTINGS permission check from AudioManager getter APIs
    Bug: 267725330
    Test: manual
    Change-Id: Ib3255900c816591ab15e23919ad0541c34563e63

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -2395,10 +2395,6 @@ public class AudioService extends IAudioService.Stub
             Log.w(TAG, "audioFormat to enable is not a surround format.");
             return false;
         }
-        if (mContext.checkCallingOrSelfPermission(android.Manifest.permission.WRITE_SETTINGS)
-                != PackageManager.PERMISSION_GRANTED) {
-            throw new SecurityException("Missing WRITE_SETTINGS permission");
-        }
 
         final long token = Binder.clearCallingIdentity();
         try {
@@ -2466,11 +2462,6 @@ public class AudioService extends IAudioService.Stub
     /** @see AudioManager#getEncodedSurroundMode() */
     @Override
     public int getEncodedSurroundMode(int targetSdkVersion) {
-        if (mContext.checkCallingOrSelfPermission(android.Manifest.permission.WRITE_SETTINGS)
-                != PackageManager.PERMISSION_GRANTED) {
-            throw new SecurityException("Missing WRITE_SETTINGS permission");
-        }
-
         final long token = Binder.clearCallingIdentity();
         try {
             synchronized (mSettingsLock) {
