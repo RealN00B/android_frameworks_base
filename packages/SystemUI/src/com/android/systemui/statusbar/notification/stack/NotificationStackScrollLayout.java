88ecb2f9503707af7f7d34fdea06c97c153ac909
Ido Ben-Hur <idoybh2@gmail.com>
2022-11-17 19:52:38 +0200

    SystemUI: Fixup edge lightning when AOD is off
    It has an offset of 1 notification as it's not yet added as a child to NotificationStackScrollLayout
    When AOD is on the child is already added so we can continue and use the same method

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -551,6 +551,8 @@ public class NotificationStackScrollLayout extends ViewGroup implements Dumpable
     private boolean mShouldUseSplitNotificationShade;
     private boolean mShowDimissButton;
 
+    private ExpandableNotificationRow mLastExpandableRow;
+
     private final ExpandableView.OnHeightChangedListener mOnChildHeightChangedListener =
             new ExpandableView.OnHeightChangedListener() {
                 @Override
@@ -737,6 +739,22 @@ public class NotificationStackScrollLayout extends ViewGroup implements Dumpable
     @ShadeViewRefactor(RefactorComponent.SHADE_VIEW)
     boolean hasActiveClearableNotifications(@SelectedRows int selection) {
         return mController.hasActiveClearableNotifications(selection);
+    }
+    
+    /** @hide */
+    public ExpandableNotificationRow getFirstActiveClearableNotifications(@SelectedRows int selection) {
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            if (!(child instanceof ExpandableNotificationRow)) {
+                continue;
+            }
+            final ExpandableNotificationRow row = (ExpandableNotificationRow) child;
+            if (row.getEntry().isClearable() && matchesSelection(row, selection)) {
+                return row;
+            }
+        }
+        return null;
     }
 
     @ShadeViewRefactor(RefactorComponent.SHADE_VIEW)
@@ -3085,8 +3103,13 @@ public class NotificationStackScrollLayout extends ViewGroup implements Dumpable
         if (child instanceof ExpandableNotificationRow) {
             ExpandableNotificationRow row = (ExpandableNotificationRow) child;
             row.setDismissUsingRowTranslationX(mDismissUsingRowTranslationX);
-
+            mLastExpandableRow = row;
         }
+    }
+
+    /** @hide */
+    public ExpandableNotificationRow getLastExpandableNotificationRow() {
+        return mLastExpandableRow;
     }
 
     @ShadeViewRefactor(RefactorComponent.COORDINATOR)
