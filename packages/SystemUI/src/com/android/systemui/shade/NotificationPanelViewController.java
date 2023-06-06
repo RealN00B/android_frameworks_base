ae35c9cbbdfc2e1de1c666e687a0ce5d3de5df17
rdx420 <padraramesh420@gmail.com>
2023-06-06 10:52:11 +0530

    Revert "SystemUI: add edge light customizations [1/2]"
    This reverts commit 83adda301bcecb4d67151baf4ff72c3871b6947f.

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -416,7 +416,6 @@ public final class NotificationPanelViewController implements Dumpable {
     private float mQuickQsHeaderHeight;
     private final ScreenOffAnimationController mScreenOffAnimationController;
     private final UnlockedScreenOffAnimationController mUnlockedScreenOffAnimationController;
-    private final EdgeLightViewController mEdgeLightViewController;
     private int mQsTrackingPointer;
     private VelocityTracker mQsVelocityTracker;
     private TrackingStartedListener mTrackingStartedListener;
@@ -826,8 +825,7 @@ public final class NotificationPanelViewController implements Dumpable {
             KeyguardTransitionInteractor keyguardTransitionInteractor,
             DumpManager dumpManager,
             TunerService tunerService,
-            Context context,
-            EdgeLightViewController edgeLightViewController) {
+            Context context) {
         keyguardStateController.addCallback(new KeyguardStateController.Callback() {
             @Override
             public void onKeyguardFadingAwayChanged() {
@@ -977,7 +975,6 @@ public final class NotificationPanelViewController implements Dumpable {
         mUnlockedScreenOffAnimationController = unlockedScreenOffAnimationController;
         mRemoteInputManager = remoteInputManager;
         mLastDownEvents = new NPVCDownEventState.Buffer(MAX_DOWN_EVENT_BUFFER_SIZE);
-        mEdgeLightViewController = edgeLightViewController;
 
         int currentMode = navigationModeController.addListener(
                 mode -> mIsGestureNavigation = QuickStepContract.isGesturalMode(mode));
@@ -1100,7 +1097,7 @@ public final class NotificationPanelViewController implements Dumpable {
                 mView.findViewById(R.id.keyguard_status_view),
                 userAvatarContainer,
                 keyguardUserSwitcherView);
-        mEdgeLightViewController.setEdgeLightView(mView.findViewById(R.id.edge_light_container));
+
         NotificationStackScrollLayout stackScrollLayout = mView.findViewById(
                 R.id.notification_stack_scroller);
         mNotificationStackScrollLayoutController.attach(stackScrollLayout);
